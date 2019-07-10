1e0f1c522d7bef645b6ecb055ceac976135b38ba
PhamHaiDang <pham.hai.dang@sun-asterisk.com>
2019-07-10 07:53:00 +0700

    Chapter4 update 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def hello
-    render html: "hello, world!"
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
