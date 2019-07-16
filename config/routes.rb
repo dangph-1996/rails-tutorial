0799d1d62693796aff10453e13a9eb06197f0802
PhamHaiDang <pham.hai.dang@sun-asterisk.com>
2019-07-16 09:33:59 +0700

    Chapter12 --v2

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,16 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
   get "users/new"
-
+  get "password_resets/new"
+  get "password_resets/edit"
   get "sessions/new"
   get "/login", to: "sessions#new"
+
   post "/login", to: "sessions#create"
+
   delete "/logout", to: "sessions#destroy"
 
   resources :users
-  resources :account_activations, only: :edit
+  resources :account_activations, only: %i(edit)
+  resources :password_resets, only: %i(new create edit update)
 end
